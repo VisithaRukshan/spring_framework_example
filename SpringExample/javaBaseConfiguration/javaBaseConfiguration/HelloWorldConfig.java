5c618271f72c7ae88a189013a57261f1281480f0
Visitha Perera <Visitha Perera@DESKTOP-OOG1RE1>
2020-06-18 11:01:19 +0530

    java base configuration (part 1)

---
+++ SpringExample/javaBaseConfiguration/javaBaseConfiguration/HelloWorldConfig.java
@@ -0,0 +1,11 @@
+package javaBaseConfiguration;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class HelloWorldConfig {
+   @Bean 
+   public HelloWorld helloWorld(){
+      return new HelloWorld();
+   }
+}
